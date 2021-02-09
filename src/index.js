8459be4e858287ebe317fab2aa6d60e39f2e3a1e
andre ferrer <andrerferrer@hotmail.com>
2021-02-09 11:11:11 -0300

    Finish in Andre's Way

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello from src/index.js!");
+document.querySelector('form').addEventListener('submit', (event) => {
+  
+  event.preventDefault();
+  const inputs = event.currentTarget.querySelectorAll('input')
+  let result = 0
+
+  inputs.forEach((input) => {
+    if (input.checked) result += parseInt(input.dataset.points)
+  });
+
+  resultDiv = document.getElementById('result');
+  resultDiv.classList.remove('ninja')
+  resultDiv.innerText = `CONGRATS! You have ${result} points`
+})
